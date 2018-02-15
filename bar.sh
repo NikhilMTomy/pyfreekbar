7d12d49a84b4531100f9a1402255264db4719fca
nikhilmtomy <nikhilmtomy@gmail.com>
2018-02-15 21:47:57 +0530

    added colors

--- bar.sh
+++ bar.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+killall lemonbar 2>&1
+
+background="#1E2127"
+foreground="#ABB2BF"
+underline="#1E2127"
+./pyfreekbar.py | lemonbar -p -g 1366x25+0+0 -B "$background" -U "$underline" -F "$foreground" -u 4 -f "FuraCode Nerd Font:style=Retina:pixelsize=11" -f "Font Awesome:pixelsize=11" 2>&1 &
